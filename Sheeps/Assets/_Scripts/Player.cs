5bbd5e31d954a45403121372b8be305d93acea44
Valentin <kibirevvalik202@gmail.com>
2021-01-08 18:35:33 +0300

    [add] Player [add] Sheeps

--- Sheeps/Assets/_Scripts/Player.cs
+++ Sheeps/Assets/_Scripts/Player.cs
@@ -7,13 +7,36 @@ public class Player : MonoBehaviour
     [SerializeField]
     private Transform _shepherd;
     private Camera _cam;
+    private Vector3 _oldPosShepherd, _startPosMouse;
+
+    private void Awake()
+    {
+        _cam = Camera.main;
+    }
     void Start()
     {
-        
     }
 
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = _cam.ScreenPointToRay(Input.mousePosition);
+            _oldPosShepherd = _shepherd.position;
+            _startPosMouse = (_cam.transform.position - ((ray.direction) *
+        ((_cam.transform.position.y - _shepherd.position.y) / ray.direction.y)));
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            Ray ray = _cam.ScreenPointToRay(Input.mousePosition);
+
+            _shepherd.position = _oldPosShepherd + (_cam.transform.position - ((ray.direction) *
+        ((_cam.transform.position.y - _shepherd.position.y) / ray.direction.y))) - _startPosMouse;
+        }
+
     }
+    //private void FixedUpdate()
+    //{
+    //    _shepherd.position = _startPosMouse;
+    //}
 }
---
+++ Sheeps/Assets/_Scripts/Sheeps.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sheeps : MonoBehaviour
+{
+    private Vector3 _direction;
+
+    [SerializeField]
+    private float _speedRuning,_speedRotation;
+    private bool _isSgepherd;
+    void Start()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        if (_isSgepherd)
+        {
+            RotationSpheeps(_direction);
+            transform.Translate(Vector3.forward * _speedRuning);
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Shepherd")
+        {
+           _direction = other.transform.position;
+            _isSgepherd = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Shepherd")
+        {
+            _direction = transform.position;
+            _isSgepherd = false;
+        }
+    }
+    private void RotationSpheeps(Vector3 PosShepherd)
+    {
+        PosShepherd.y = transform.position.y;
+        Vector3 direction = (transform.position - PosShepherd).normalized;
+        transform.forward = Vector3.Slerp(transform.forward,direction,_speedRotation);
+    }
+}
