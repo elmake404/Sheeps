6db11caa51be0215fd9afdffd6b22de6eadef7d9
Valentin <kibirevvalik202@gmail.com>
2021-01-08 16:14:46 +0300

    [add] Project

---
+++ Sheeps/Assets/_Scripts/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _shepherd;
+    private Camera _cam;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
