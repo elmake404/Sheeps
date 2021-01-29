0cfe0e7eaa7edb04061dbcf467d9622d83d19779
Valentin <kibirevvalik202@gmail.com>
2021-01-29 15:17:36 +0300

    [update] GameContinue

--- Sheeps/Assets/_Scripts/Canvas/GameContinue.cs
+++ Sheeps/Assets/_Scripts/Canvas/GameContinue.cs
@@ -10,7 +10,8 @@ public class GameContinue : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             CanvasManager.IsWinGame = false;
-            if (PlayerPrefs.GetInt("Scenes") < SceneManager.sceneCountInBuildSettings - 1)
+
+            if (PlayerPrefs.GetInt("Scenes") < SceneManager.sceneCountInBuildSettings)
             {
                 SceneManager.LoadScene(PlayerPrefs.GetInt("Scenes"));
             }
